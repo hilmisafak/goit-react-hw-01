df0e2f236f81629ef3d711d0e988cae27faf5488
HilmiSafak <hilmisafak1@gmail.com>
2025-09-18 23:55:56 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Profile from "./components/Profile/Profile";
+import Profile from "./components/profile/Profile";
 import FriendList from "./components/FriendList/FriendList";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
